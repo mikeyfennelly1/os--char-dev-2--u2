b341204cd1335c18439ed53cb75617a042dd5c77
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2025-03-16 11:44:39 +0000

    simple client created

---
+++ src/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	// Define the server address and port
+	serverAddr := "localhost:8989"
+
+	// Connect to the server
+	conn, err := net.Dial("tcp", serverAddr)
+	if err != nil {
+		fmt.Println("Error connecting:", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	fmt.Println("Connected to server")
+
+	// Read data from the server
+	reader := bufio.NewReader(conn)
+	for {
+		message, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading:", err)
+			break
+		}
+		fmt.Print("Received: ", message)
+	}
+}
